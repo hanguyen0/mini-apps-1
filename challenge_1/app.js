9f389d89aeff011f6da1bd4a7ba88703c31f09fc
uxsasukexnaruto <nguyha16@gmail.com>
2020-04-20 15:03:14 -0700

    refactor into separation of concern

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -22,75 +22,74 @@ let thirdRow = document.getElementById('third').querySelectorAll('td');
 //   });
 // });
 
-
-
 let board = [firstRow, secondRow, thirdRow]
 let clicked = false;
 let announcement = document.querySelector('h2');
 let validBoard = [];
-let winByRow;
+let player1 = 'X';
+let player2 = 'O';
+let winByRow = '';
+// let winByRow;
 //checking for winner
-board.forEach(row => {
-  //listen for click event each row
-  winByRow = '';
-  let player1 = 0;
-  let player2 = 0;
-  row.forEach(cell => {
-
-    cell.addEventListener('click', (event) => {
-      //rules of Tic Tag Toe, make sure cell is empty before placing X or O
-      if (event.target.innerHTML.length === 0) {
-        if (!clicked) {
-          event.target.innerHTML = 'X';
-          winByRow += 'X';
-          player1++;
-          console.log('player1', player1)
-          console.log(winByRow)
-          clicked = true;
-        } else {
-          event.target.innerHTML = 'O';
-          winByRow += 'O';
-          player2++;
-          console.log('player2', player2);
-          console.log(winByRow)
-          clicked = false;
-        }
-        if (winByRow.length === 3) {
-          validBoard.push(winByRow);
-          console.log('valid board: ', validBoard);
-        }
-        if (player1 === 3) {
-          announcement.textContent = 'WInner is X';
-        } else if (player2 === 3) {
-          announcement.textContent = 'Winner is O';
-        }
-      }
-    });
-
-  });
 
 
 
-});
+// if (winByRow.length === 3) {
+//   validBoard.push(winByRow);
+//   console.log('valid board: ', validBoard);
+// }
+// if (player1 === 3) {
+//   announcement.textContent = 'WInner is X';
+// } else if (player2 === 3) {
+//   announcement.textContent = 'Winner is O';
+// }
 
 
+//handle cell click
+cellClick = (event) => {
+  //rules of Tic Tag Toe, make sure cell is empty before placing X or O
+  if (event.target.innerHTML.length === 0) {
+    if (!clicked) {
+      event.target.innerHTML = 'X';
+      winByRow += 'X';
+      player1++;
+      console.log('player1', player1)
+      console.log(winByRow)
+      clicked = true;
+    } else {
+      event.target.innerHTML = 'O';
+      winByRow += 'O';
+      player2++;
+      console.log('player2', player2);
+      console.log(winByRow)
+      clicked = false;
+    }
+  }
 
-checkBoard = (board) => {
+  //validate board
+  checkBoard = (board) => {
 
-}
-
-
-
+  }
 
 
-//reset game:
-
-let reset = document.querySelector('button');
-reset.addEventListener('click', (e) => {
-  winByRow = '';
   board.forEach(row => {
+    //listen for click event each row
     row.forEach(cell => {
-      cell.textContent = '';
-    })
+      cell.addEventListener('click', cellClick);
+
+    });
   });
-});
+
+
+  //reset game:
+
+  let reset = document.querySelector('button');
+  reset.addEventListener('click', (e) => {
+    // winByRow = '';
+    announcement.textContent = '';
+    board.forEach(row => {
+      row.forEach(cell => {
+        cell.textContent = '';
+      })
+    });
+  });
