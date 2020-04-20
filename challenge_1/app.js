0c4934ae3d5d850f6ac808800df47d044e31fe0a
uxsasukexnaruto <nguyha16@gmail.com>
2020-04-20 11:01:03 -0700

    make a table and add css to the table

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,13 @@
+let firstRow = document.getElementById('first');
+let secondRow = document.getElementById('second');
+let thirdRow = document.getElementById('third');
 
+let cell = document.querySelectorAll("td");
+// console.log(td);
 
+for (let i = 0; i < cell.length; i++) {
+  // console.log(cell[i]);
+  // cell[i].addEventListenter('click', (e) => {
+  //   console.log("Hello");
+  // })
+}
