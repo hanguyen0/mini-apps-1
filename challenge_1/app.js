5ab213123b0151d4ab2c0a87961f77ee7bfc7f77
uxsasukexnaruto <nguyha16@gmail.com>
2020-04-20 16:01:08 -0700

    still need to figure out winning logics for column and diagonals

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -2,35 +2,11 @@
 let firstRow = document.getElementById('first').querySelectorAll('td');
 let secondRow = document.getElementById('second').querySelectorAll('td');
 let thirdRow = document.getElementById('third').querySelectorAll('td');
-
-// //adding X to each cell clicked
-// let cells = document.querySelectorAll("td");
-//flag to alternating between X and O
-// let clicked = false;
-// cells.forEach(cell => {
-//   cell.addEventListener('click', (event) => {
-//     //rules of Tic Tag Toe, make sure cell is empty before placing X or O
-//     if (event.target.innerHTML.length === 0) {
-//       if (!clicked) {
-//         event.target.innerHTML = 'X';
-//         clicked = true;
-//       } else {
-//         event.target.innerHTML = 'O';
-//         clicked = false;
-//       }
-//     }
-//   });
-// });
-
 let board = [firstRow, secondRow, thirdRow]
 let clicked = false;
 let announcement = document.querySelector('h2');
 let validBoard = [];
-let player1 = 'X';
-let player2 = 'O';
-let winByRow = '';
-// let winByRow;
-//checking for winner
+let firstPlay = 'X';
 
 
 
@@ -38,54 +14,28 @@ let winByRow = '';
 //   validBoard.push(winByRow);
 //   console.log('valid board: ', validBoard);
 // }
-// if (player1 === 3) {
-//   announcement.textContent = 'WInner is X';
-// } else if (player2 === 3) {
-//   announcement.textContent = 'Winner is O';
+
+
+
+//check board for winner
+// checkBoard = (row) => {
+//   row.childNodes.forEach(cell => {
+//     console.log(cell.textContent);
+//   })
 // }
 
 
-//handle cell click
-cellClick = (event) => {
-  //rules of Tic Tag Toe, make sure cell is empty before placing X or O
-  if (event.target.innerHTML.length === 0) {
-    if (!clicked) {
-      event.target.innerHTML = 'X';
-      winByRow += 'X';
-      player1++;
-      console.log('player1', player1)
-      console.log(winByRow)
-      clicked = true;
-    } else {
-      event.target.innerHTML = 'O';
-      winByRow += 'O';
-      player2++;
-      console.log('player2', player2);
-      console.log(winByRow)
-      clicked = false;
-    }
-  }
-
-  //validate board
-  checkBoard = (board) => {
-
-  }
-
-
-  board.forEach(row => {
-    //listen for click event each row
-    row.forEach(cell => {
-      cell.addEventListener('click', cellClick);
-
-    });
-  });
-
-
+board.forEach(row => {
+  let winByRow = '';
+  let player1 = 0;
+  let player2 = 0;
   //reset game:
-
   let reset = document.querySelector('button');
   reset.addEventListener('click', (e) => {
-    // winByRow = '';
+    winByRow = '';
+    clicked = false;
+    player1 = 0;
+    player2 = 0;
     announcement.textContent = '';
     board.forEach(row => {
       row.forEach(cell => {
@@ -93,3 +43,38 @@ cellClick = (event) => {
       })
     });
   });
+  if (winByRow.length === 3) {
+    validBoard.push(winByRow);
+  }
+
+
+  row.forEach((cell, idx )=> {
+    cell.addEventListener('click', (event) => {
+      let target = event.target;
+      //rules of Tic Tag Toe, make sure cell is empty before placing X or O
+      if (target.textContent === "") {
+        if (!clicked) {
+          target.textContent = 'X';
+          winByRow += 'X';
+          player1++;
+          // console.log('player1', player1)
+          console.log(winByRow)
+          clicked = true;
+        } else {
+          target.textContent = 'O';
+          winByRow += 'O';
+          player2++;
+          // console.log('player2', player2);
+          console.log(winByRow)
+          clicked = false;
+        }
+        if (player1 === 3) {
+          announcement.textContent = 'WInner is X';
+        } else if (player2 === 3) {
+          announcement.textContent = 'Winner is O';
+        }
+      }
+    });
+  });
+});
+
