57ba9688e7cde7795c5249dc0431b03418423b41
uxsasukexnaruto <nguyha16@gmail.com>
2020-04-20 13:41:19 -0700

    figuring out how to check for winner, losing or draw board

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,13 +1,76 @@
-let firstRow = document.getElementById('first');
-let secondRow = document.getElementById('second');
-let thirdRow = document.getElementById('third');
 
-let cell = document.querySelectorAll("td");
-// console.log(td);
+let firstRow = document.getElementById('first').querySelectorAll('td');
+let secondRow = document.getElementById('second').querySelectorAll('td');
+let thirdRow = document.getElementById('third').querySelectorAll('td');
 
-for (let i = 0; i < cell.length; i++) {
-  // console.log(cell[i]);
-  // cell[i].addEventListenter('click', (e) => {
-  //   console.log("Hello");
-  // })
+// //adding X to each cell clicked
+// let cells = document.querySelectorAll("td");
+//flag to alternating between X and O
+// let clicked = false;
+// cells.forEach(cell => {
+//   cell.addEventListener('click', (event) => {
+//     //rules of Tic Tag Toe, make sure cell is empty before placing X or O
+//     if (event.target.innerHTML.length === 0) {
+//       if (!clicked) {
+//         event.target.innerHTML = 'X';
+//         clicked = true;
+//       } else {
+//         event.target.innerHTML = 'O';
+//         clicked = false;
+//       }
+//     }
+//   });
+// });
+
+
+
+let board = [firstRow, secondRow, thirdRow]
+let clicked = false;
+let winByCol = 0;
+//checking for winner
+board.forEach(row => {
+  let winByRow = 0;
+
+  //listen for click event each row
+  row.forEach(cell => {
+    cell.addEventListener('click', (event) => {
+      //rules of Tic Tag Toe, make sure cell is empty before placing X or O
+      if (event.target.innerHTML.length === 0) {
+        if (!clicked) {
+          event.target.innerHTML = 'X';
+          winByRow++;
+          winByCol++;
+          console.log(winByRow)
+          clicked = true;
+        } else {
+          event.target.innerHTML = 'O';
+          winByRow--;
+          winByCol++;
+          console.log(winByRow)
+          clicked = false;
+        }
+        if (winByRow === 3) {
+          console.log('WInner is ...');
+        } else {
+          console.log('No winner...')
+        }
+      }
+    });
+
+  });
+
+
+
+});
+
+
+
+checkRow = (row) => {
+  
 }
+
+
+
+
+
+//reset game:
