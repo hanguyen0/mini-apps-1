0bfaaaf398bc417db1e6644e7ed76f3f1826b75f
uxsasukexnaruto <nguyha16@gmail.com>
2020-04-22 09:39:59 -0700

    got server running, post and get request are working. Need to work on form submission data

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,12 @@
 
+let formEl = document.querySelector('form');
 
+formEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // const formData = new FormData(e.target);
+    console.log(e);
+    // submit the data via XHR
+    //   let request = new XMLHttpRequest();
+    //   request.open("POST", "/formHandler");
+    //   request.send(data);
+});
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.use(express.json());
+app.use(express.static('client'));
 
+app.get('/data', (req, res) => {
+    console.log(res)
+    res.send("hello there");
+});
+
+app.post('/data', (req, res) => {
+    console.log(req.body)
+    res.send("got a Post request");
+})
+app.listen(port, () => console.log(`App listening on port ${port}`));
