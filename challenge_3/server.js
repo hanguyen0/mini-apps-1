01e976f1979334bb261b0b170486f8f2286cf13a
uxsasukexnaruto <nguyha16@gmail.com>
2020-04-22 11:27:26 -0700

    set up server and babel to work correctly

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use(express.static('public'));
+app.use(express.json());
 
 
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+})
+
+app.listen(PORT, () => console.log('Listerning on port' + PORT));
